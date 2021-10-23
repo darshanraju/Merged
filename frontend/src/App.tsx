fd68fc94aea51fd1bbbbf3458c1a834ec9f2ec23
Darshan <darshan.raju99@gmail.com>
2021-10-24 09:05:26 +1100

    Added web3 package

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,15 @@
 import { Grid, Typography } from "@material-ui/core";
 import { createStyles, makeStyles } from "@material-ui/styles";
-import React from "react";
+import React, { useEffect } from "react";
+import Web3 from "web3";
 import "./App.css";
 import Hero from "./components/Hero";
 // import StickyHeadTable from "./components/Issues";
 import IssuesTable from "./components/IssuesTable";
 import Statistics from "./components/Statistics";
 import { theme } from "./providers/ThemeProvider";
+
+declare let window: any;
 
 const styles = makeStyles(() =>
   createStyles({
@@ -24,6 +27,26 @@ const styles = makeStyles(() =>
 
 const App = () => {
   const classes = styles();
+
+  useEffect(() => {
+    const ropsteinEndpoint =
+      "wss://ropsten.infura.io/ws/v3/1f8da82842b144fd8b0677f547a37182";
+
+    const web3 = new Web3(ropsteinEndpoint);
+
+    let provider = window.ethereum;
+
+    if (typeof provider !== "undefined") {
+      provider
+        .request({ method: "eth_requestAccounts" })
+        .then((accounts: any) => {
+          console.log("accounts: ", accounts);
+        })
+        .catch((error: any) => {
+          console.log("Error: ", error);
+        });
+    }
+  }, []);
 
   return (
     <div className={classes.root}>
